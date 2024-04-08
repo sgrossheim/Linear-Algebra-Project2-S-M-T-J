2f35753d8517f4a0be84999407777112fb823f6e
Montana Merkle <143360914+m-merkle@users.noreply.github.com>
2024-04-08 17:41:06 -0500

    Update LightingGame1Pt2.cpp

--- LightingGame1Pt2.cpp
+++ LightingGame1Pt2.cpp
@@ -22,12 +22,7 @@ public:
        Switches(); //constructor that sets switches to user input
        int CheckIn(char letter); //checks user input of switch state
        void Print() const; //Print method
-       void Swap(bool& spefswitch); //swap state of switch
-       void SwitchA();
-       void SwitchB();
-       void SwitchC();
-       void SwitchD();
-       void SwitchE();
+       bool Solvable(int& numA, int& numB, int& numC, int& numD, int& numE);
 };     
 
 //*****************************************
@@ -79,59 +74,112 @@ int Switches::CheckIn(char letter){
 
 //outputs current state of switches
 void Switches::Print() const{
+	cout << endl;
 	cout << "Current State of Switches: " << endl;
     cout << "A    B    C    D    E" << endl;
     cout << A << "    " << B << "    " << C << "    " << D << "    " << E << endl;
 }
 
-void Switches::Swap(bool& spefswitch){
-	if (spefswitch == false)
-		spefswitch = true;
-	else
-		spefswitch = false;
+//determines if configuration is solvable
+bool Switches::Solvable(int& numA, int& numB, int& numC, int& numD, int& numE){
+	//get number of lights on
+	int count = 0;
+	if (A == true)
+		count++;
+	if (B == true)
+		count++;
+	if (C == true)
+		count++;
+	if (D == true)
+		count++;
+	if (E == true)
+		count++;
+		
+	// if count is even
+	if ((count % 2) == 0){
+		//if c is on
+		if(C == true)
+			return false;
+		//if a and b are on
+		if(((A == true) && (B == true)) || ((D == true) && (E == true))){
+			//determine if grouping results in 2 or 1 turns
+			if(count == 4){
+				numA = 1;
+				numE = 1;
+			}
+			else
+				if(A == true)
+					numA = 1;
+				else
+					numE = 1;	
+			return true;
+		}
+	}
+	
+	//if only one switch on
+	else if (count == 1)
+		return false;
+		
+	//if odd and grouped together (three in a row) or (10101)
+	else if(((A == true)&&(B == true)&&(C == true)) || ((B == true)&&(C == true)&&(D == true)) || ((C == true)&&(D == true)&&(E == true)) || ((A == true)&&(C == true)&&(E == true))){
+		//determine number of turns - either all on or group of three
+		if (count == 5){
+			numB = 1;
+			numE = 1;
+		}
+		else
+			//if grouping of three of ABC
+			if((A == true) && (B == true))
+				numB = 1;
+			//if grouping of three of BCD
+			else if((B == true) && (C == true))
+				numC = 1;
+			//if three where ACE are on
+			else if((A == true) && (C == true)){
+				numA = 1;
+				numE = 1;
+				numC = 1;
+			}
+			//if grouping of three of CDE
+			else
+				numD = 1;
+		return true;
+	}
+	
+	//if they're already all off, dont change turns and stays at 0
+	else if (count == 0)
+		return true;
+	
+	//everything else: false
+	return false;
 }
-
-void Switches::SwitchA(){
-	Swap(A);
-	Swap(B);
-	Print();
-}
-
-void Switches::SwitchB(){
-	Swap(B);
-	Swap(A);
-	Swap(C);
-	Print();
-}
-
-void Switches::SwitchC(){
-	Swap(C);
-	Swap(B);
-	Swap(D);
-	Print();
-}
-
-void Switches::SwitchD(){
-	Swap(D);
-	Swap(C);
-	Swap(E);
-	Print();
-}
-
-void Switches::SwitchE(){
-	Swap(E);
-	Swap(D);
-	Print();
-}
-
-
 
 int main(){
+	//define local variables
+	bool result;
+	int numA = 0;
+    int numB = 0;
+    int numC = 0;
+    int numD = 0;
+    int numE = 0;
+	
     //instantiation
     Switches panel;
-    //panel.Print();
-    //cout << endl;
+    result = panel.Solvable(numA, numB, numC, numD, numE);
+    cout << endl;
     
+    //output results
+    if (result == 1){
+    	cout << "This configuration is solvable" << endl;
+		cout << "Turns needed for each switch: " << endl;
+		cout << "A: " << numA << endl;
+		cout << "B: " << numB << endl;
+		cout << "C: " << numC << endl; 
+		cout << "D: " << numD << endl;
+		cout << "E: " << numE << endl;
+	}
+	else
+    	cout << "This configuration is not solvable." << endl;
     
     return 0;
 }
